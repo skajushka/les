f5cb7440b9768f53d790c07b70f2bcf635ea0949
skajushka <hlinistaja@gmail.com>
2017-11-05 23:35:12 +0300

    Updated Forest.

--- src/main/java/com/forest/Forest.java
+++ src/main/java/com/forest/Forest.java
@@ -1,12 +1,15 @@
 package com.forest;
 
 import com.forest.lifeform.LifeForm;
+import com.forest.lifeform.animal.Animal;
+import com.forest.lifeform.plant.Plant;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Forest {
 
-    private List<LifeForm> biotope;
+    private List<LifeForm> biotope = new ArrayList<LifeForm>();
 
     public void setBiotope(List<LifeForm> biotope) {
         this.biotope = biotope;
@@ -16,7 +19,37 @@ public class Forest {
         return biotope;
     }
 
+    public List<Plant> getFlora() {
+        List<Plant> flora = new ArrayList<Plant>();
+
+        for (LifeForm lifeForm : biotope) {
+            if (lifeForm instanceof Plant) {
+                flora.add((Plant) lifeForm);
+            }
+        }
+
+        return flora;
+    }
+
+    public List<Animal> getFauna() {
+        List<Animal> fauna = new ArrayList<Animal>();
+
+        for (LifeForm lifeForm : biotope) {
+            if (lifeForm instanceof Animal) {
+                fauna.add((Animal) lifeForm);
+            }
+        }
+
+        return fauna;
+    }
+
     public void addLifeform(LifeForm lifeForm) {
         biotope.add(lifeForm);
+    }
+
+    public String toString() {
+        String result = "";
+        result += "There are " + getFauna().size() + " animals and " + getFlora().size() + " plants in the forest." ;
+        return result;
     }
 }
--- src/main/java/com/forest/Main.java
+++ src/main/java/com/forest/Main.java
@@ -1,40 +1,38 @@
 package com.forest;
 
+import com.forest.lifeform.LifeForm;
 import com.forest.lifeform.animal.*;
 import com.forest.lifeform.plant.Heather;
 import com.forest.lifeform.plant.Pine;
 import com.forest.lifeform.plant.Plant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Animal pipka = new DomesticCat("Pipka", "tricolor", 'F', 3);
-		process(pipka);
-		System.out.println();
 		Animal charka = new ZwergSchnautzer("Charka", "black", 'M', 7);
-		process(charka);
-		charka.play(pipka);
-		System.out.println();
 		Plant pine = new Pine(2, 20);
-		process(pine);
-		System.out.println();
 		Plant heather = new Heather(1,10);
-		process(heather);
-        System.out.println();
 		Racoon inocek = new Racoon("Inocek", "grey", 'F', 3);
-		inocek.introduce();
-		inocek.pickFlowers();
+
+		Forest forest = new Forest();
+		System.out.println(forest);
+		forest.setBiotope(getInhabitants(pipka, pine));
+		System.out.println(forest);
+		forest.addLifeform(heather);
+		System.out.println(forest);
+		forest.addLifeform(inocek);
+		forest.addLifeform(charka);
+		System.out.println(forest);
 	}
-			
-	public static void process(Animal animal) {
-		animal.introduce();
-		animal.voice();
-		animal.pooPoo();
-		animal.checkHunger();
-		animal.feed();
-	}
-	
-	public static void process(Plant plant) {
-		plant.introduce();
+
+	public static List<LifeForm> getInhabitants(LifeForm pipka, LifeForm pine){
+		List<LifeForm> inhabitants = new ArrayList<LifeForm>();
+		inhabitants.add(pipka);
+		inhabitants.add(pine);
+		return inhabitants;
 	}
 }
