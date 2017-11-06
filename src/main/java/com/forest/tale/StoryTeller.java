0ee02d010ef68b0041f1c8f5e44fccf67c2fc275
skajushka <hlinistaja@gmail.com>
2017-11-06 19:06:40 +0300

    Moved messages to properties file.

--- src/main/java/com/forest/tale/StoryTeller.java
+++ src/main/java/com/forest/tale/StoryTeller.java
@@ -1,12 +1,36 @@
 package com.forest.tale;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class StoryTeller {
 
+    private static String BEGIN_STORY = "story.begin";
+    private static String END_STORY = "story.end";
+    private static String RESOURCE_PATH = "storyTeller/messages.properties";
+
+    private Properties messages;
+
+    public StoryTeller() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        Properties properties = new Properties();
+
+        try(InputStream resourceStream = classloader.getResourceAsStream(RESOURCE_PATH)) {
+            properties.load(resourceStream);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        this.messages = properties;
+    }
+
     public void beginTheStory() {
-        System.out.println("Once upon a time there was a forest.");
+        System.out.println(messages.get(BEGIN_STORY));
     }
 
     public void endTheStory() {
-        System.out.println("Happy end.");
+        System.out.println(messages.get(END_STORY));
     }
 }
