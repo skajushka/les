3206b4a77fdf6c716cff313c46fa701c67bde849
skajushka <hlinistaja@gmail.com>
2017-11-06 12:40:27 +0300

    StoryTeller added.

--- src/main/java/com/forest/Main.java
+++ src/main/java/com/forest/Main.java
@@ -1,20 +1,16 @@
 package com.forest;
 
-import com.forest.lifeform.LifeForm;
-import com.forest.lifeform.animal.*;
-import com.forest.lifeform.plant.Heather;
-import com.forest.lifeform.plant.Pine;
-import com.forest.lifeform.plant.Plant;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.forest.tale.StoryTeller;
 
 public class Main {
 
 	private static ForestService forestService = new ForestService();
+    private static StoryTeller storyTeller = new StoryTeller();
 
 	public static void main(String[] args) {
 		Forest forest = forestService.createForest();
+        storyTeller.beginTheStory();
 		System.out.println(forest);
+		storyTeller.endTheStory();
 	}
 }
---
+++ src/main/java/com/forest/tale/StoryTeller.java
@@ -0,0 +1,12 @@
+package com.forest.tale;
+
+public class StoryTeller {
+
+    public void beginTheStory() {
+        System.out.println("Once upon a time there was a forest.");
+    }
+
+    public void endTheStory() {
+        System.out.println("Happy end.");
+    }
+}
