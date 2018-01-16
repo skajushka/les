3f372ef2b41dda91d15c19c553639bf87444b775
skajushka <hlinistaja@gmail.com>
2018-01-16 15:26:58 +0300

    Updated Story

--- src/main/java/com/forest/tale/Story.java
+++ src/main/java/com/forest/tale/Story.java
@@ -6,14 +6,35 @@ import com.forest.ForestService;
 import com.forest.lifeform.animal.AnimalService;
 import com.forest.lifeform.plant.PlantService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 public class Story {
 
+    private static String RESOURCE_PATH = "storyTeller/messages.properties";
+    private static String ADDING_FOREST = "adding.forest";
+    private static String FIRST_VAR = "first.variant";
+    private static String SECOND_VAR = "second.variant";
+    private Properties messages;
     private ConsoleReader consoleReader;
     private StoryTeller storyTeller;
 
     public Story() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Properties properties = new Properties();
+
+        try (InputStream resourceStream = classLoader.getResourceAsStream(RESOURCE_PATH)) {
+            properties.load(resourceStream);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
         this.consoleReader = new ConsoleReader();
         this.storyTeller = new StoryTeller();
+        this.messages = properties;
     }
 
     public void start() {
@@ -27,7 +48,12 @@ public class Story {
         ForestService forestService = new ForestService(new PlantService(), new AnimalService());
         Forest forest = forestService.createForest();
         System.out.println(forest);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(messages.get(ADDING_FOREST));
+        System.out.println(messages.get(FIRST_VAR));
+        System.out.println(messages.get(SECOND_VAR));
     }
+
 
     public void end() {
         storyTeller.endTheStory();
